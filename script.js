53217902f368e31d47d246b53b9256c21fa0364e
saiteja-143 <allasaiteja8@gmail.com>
2022-04-17 11:49:47 +0530

    version1.0

---
+++ script.js
@@ -0,0 +1,146 @@
+"use strict";
+// let dataList = document.getElementById("countries");
+// let count = 0;
+
+const url = "https://restcountries.com/v3.1/all";
+let display = {};
+// const url1 = `https://api.agify.io?name=${Name}&country_id=${country}`;
+
+let div1 = document.createElement("div");
+div1.className = "search-container";
+
+let input1 = document.createElement("input");
+input1.type = "text";
+input1.value = "Enter the person name";
+
+div1.appendChild(input1);
+
+let input2 = document.createElement("input");
+input2.setAttribute("list", "countries");
+input2.type = "text";
+input2.value = "Select Country";
+
+let dataList = document.createElement("datalist");
+dataList.id = "countries";
+input2.appendChild(dataList);
+
+div1.appendChild(input2);
+
+let search = document.createElement("button");
+search.innerHTML = "Find";
+search.id = "finding";
+div1.appendChild(search);
+
+document.body.appendChild(div1);
+
+// Creating the Input tags using DOM
+fetch(url)
+  .then((response) => response.json())
+  .then((data) => {
+    data.forEach((element) => {
+      let dataList1 = document.getElementById("countries");
+      //   console.log(element.name.common);
+      let options = document.createElement("option");
+      options.value = element.cca2;
+      options.innerHTML = element.name.common;
+      dataList1.appendChild(options);
+      //   console.log(element.cca2);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+//Event that occurs after the Clicking on Find Button
+search.addEventListener("click", function () {
+  let Name = input1.value;
+  let country = input2.value;
+
+  //If condition
+  if (country == "Select Country") {
+    let newurl1 = `https://api.nationalize.io?name=${Name}`;
+    fetch(newurl1)
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.country.length === 0) {
+          alert("Select the country Name with person name");
+          location.reload();
+        } else {
+          country = data.country[0].country_id;
+          let ageurl = `https://api.agify.io?name=${Name}&country_id=${country}`;
+          fetch(ageurl)
+            .then((response) => response.json())
+            .then((data) => {
+              if (data.age === null) {
+                alert("Enter the name properly");
+              } else {
+                display.Name = data.name;
+                display.countryid = data.country_id;
+                display.Age = data.age;
+
+                let genderurl = `https://api.genderize.io?name=${Name}&country_id=${country}`;
+                fetch(genderurl)
+                  .then((response) => response.json())
+                  .then((data) => {
+                    if (data.gender === null) {
+                      alert("Enter the proper name");
+                      location.reload();
+                    } else {
+                      display.gender = data.gender;
+                      display.gender_probability = data.probability;
+                      display.records_examined = data.count;
+                    }
+                  })
+                  .catch((err) => {
+                    console.log(err);
+                  });
+              }
+            })
+            .catch((err) => {
+              console.log(err);
+            });
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  //Else condition
+  else {
+    let ageurl = `https://api.agify.io?name=${Name}&country_id=${country}`;
+    fetch(ageurl)
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.age === null) {
+          alert("Enter the Person Name and Country  properly");
+          location.reload();
+        } else {
+          display.Name = data.name;
+          display.countryid = data.country_id;
+          display.age = data.age;
+
+          let genderurl = `https://api.genderize.io?name=${Name}&country_id=${country}`;
+          fetch(genderurl)
+            .then((response) => response.json())
+            .then((data) => {
+              if (data.gender === null) {
+                alert("Enter the proper name");
+                location.reload();
+              } else {
+                display.gender = data.gender;
+                display.gender_probability = data.probability;
+                display.records_examined = data.count;
+              }
+            })
+            .catch((err) => {
+              console.log(err);
+            });
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+  console.log(display);
+});
